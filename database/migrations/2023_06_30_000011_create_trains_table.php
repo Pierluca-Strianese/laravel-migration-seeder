e2345af02b18336fa1e56de01a6911f2a7c6bb2f
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-06-30 15:20:37 +0200

    COMPLETE

---
+++ database/migrations/2023_06_30_000011_create_trains_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('company');
+            $table->char('train_code', 8);
+            $table->string('departure_station', 30);
+            $table->string('arrival_station', 30);
+            $table->time('departure_time');
+            $table->date('departure_date');
+            $table->time('arrival_time');
+            $table->date('arrival_date');
+            $table->tinyinteger('number_of_carriages');
+            $table->boolean('on_time');
+            $table->boolean('deleted');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
